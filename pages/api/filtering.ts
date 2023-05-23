12947f171eb7ba4b917bc52a9e3b5275f09977fa
Your Name <you@example.com>
2023-05-23 13:07:38 -0600

    initial comit

--- pages/api/filtering.ts
+++ pages/api/filtering.ts
@@ -29,7 +29,7 @@ const filter = async (req: Request): Promise<Response> => {
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 1500,
+    max_tokens: 3000,
     stream: true,
     n: 1,
   };
