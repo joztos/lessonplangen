5f7284d76b9c836cd44ca7167004a4fd15305960
Your Name <you@example.com>
2023-05-23 16:24:23 -0600

    footer change

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
-        Powered by{" "}
+        Created by Samasat Group for EMDI school{" "}
         <a
           href="https://samasat.com/"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Samasat AI{" "}
+          Powered via{" "}
         </a>
-        and{" "}
+        Navi AI{" "}
         <a
           href="https://samasat.com/"
           target="_blank"
