8d7d2c072e06e6cd347d01d0550a613120edcf1a
Your Name <you@example.com>
2023-07-17 22:09:49 -0600

    droop good

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -1,34 +1,42 @@
-function App() {
-  const niveles: OptionType[] = [
-    { label: "Elemental", value: "elemental" },
-    { label: "Media", value: "media" },
-    { label: "Superior", value: "superior" },
-    { label: "Bachillerato General Unificado", value: "bachillerato" },
-  ];
+import { useState } from "react";
 
-  const otrosFiltros: OptionType[] = [
-    { label: "Opción 1", value: "1" },
-    { label: "Opción 2", value: "2" },
-    { label: "Opción 3", value: "3" },
-    // más opciones...
-  ];
+export type OptionType = {
+  label: string;
+  value: string;
+}
 
-  const [nivelSeleccionado, setNivelSeleccionado] = useState(niveles[0]);
-  const [filtroSeleccionado, setFiltroSeleccionado] = useState(otrosFiltros[0]);
+type DropDownProps = {
+  options: OptionType[];
+  selectedOption: OptionType;
+  setSelectedOption: (option: OptionType) => void;
+  label: string;  // Añade esta línea
+};
+
+const DropDown: React.FC<DropDownProps> = ({ options, selectedOption, setSelectedOption, label }) => {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <DropDown 
-        options={niveles} 
-        selectedOption={nivelSeleccionado} 
-        setSelectedOption={setNivelSeleccionado} 
-      />
-      <DropDown 
-        options={otrosFiltros} 
-        selectedOption={filtroSeleccionado} 
-        setSelectedOption={setFiltroSeleccionado} 
-      />
-      {/* resto de tu código... */}
+    <div className="relative inline-block text-left">
+      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">{label}</label>  {/* Muestra la etiqueta aquí */}
+      <div>
+        <button type="button" onClick={() => setOpen(!open)}>
+          {selectedOption.label}
+        </button>
+      </div>
+
+      {open && (
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+            {options.map((option) => (
+              <button key={option.value} onClick={() => {setSelectedOption(option); setOpen(false);}} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                {option.label}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default DropDown;
