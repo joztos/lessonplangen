2d116a774d4acb58d30c29ec431b610777d1c95b
Your Name <you@example.com>
2023-07-17 22:32:27 -0600

    a12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,46 @@
-import type { NextPage } from "next";
+// Import necessary modules
+import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useRef, useState, useEffect } from "react";
 import { Toaster, toast } from "react-hot-toast";
-import DropDown, { VibeType } from "../components/DropDown";
+import DropDown, { OptionType } from "../components/DropDown";
 import Footer from "../components/Footer";
 import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 import parse from "html-react-parser";
 
+const gradeLevels: OptionType[] = [
+  { label: 'Elemental', value: 'Elemental' },
+  { label: 'Media', value: 'Media' },
+  { label: 'Superior', value: 'Superior' },
+  { label: 'Bachillerato General Unificado', value: 'Bachillerato General Unificado' },
+];
+
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Primero de Primaria");
-  const [generatedBios, setGeneratedBios] = useState<string>("");
+  const [lessonPlan, setLessonPlan] = useState("");
+  const [gradeLevel, setGradeLevel] = useState<OptionType>(gradeLevels[0]);
+  const [generatedLessonPlans, setGeneratedLessonPlans] = useState<string>("");
 
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const lessonPlanRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: "smooth" });
+  const scrollToLessonPlans = () => {
+    if (lessonPlanRef.current !== null) {
+      lessonPlanRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   useEffect(() => {
-    console.log(generatedBios);
-  }, [generatedBios]);
+    console.log(generatedLessonPlans);
+  }, [generatedLessonPlans]);
 
   const prompt =
-    "Estoy creando una aplicación que genera planeaciones de clase para estudiantes. Contamos con varios niveles de grado de estudiantes, como Primero de Primaria, Segundo de Primaria.. Todos ellos tienen su propio tema para crear una planeación de clase. Por favor, crea un planeación de clase larga y perfecta de acuerdo al nivel de grado del estudiante y al tema..  grado del estudiante es " +
-    vibe +
+    "Estoy creando una aplicación que genera planeaciones de clase para estudiantes. Contamos con varios niveles de grado de estudiantes, como Elemental, Media, Superior, Bachillerato General Unificado.. Todos ellos tienen su propio tema para crear una planeación de clase. Por favor, crea un planeación de clase larga y perfecta de acuerdo al nivel de grado del estudiante y al tema..  grado del estudiante es " +
+    gradeLevel.label +
     " el tema es " +
-    bio +
+    lessonPlan +
     `\n. Your response must be formatted using HTML Elements for easier readability, including paragraph tags, line breaks, headings and bold titles where applicable, no need to create Full HTML Page including head, title elements. Escriba el contenido anterior con los temas siguientes.    
     1. Actividad temprana.
     Proporcione ejemplos de actividades de juego motivadoras, como una canción de atención, un poco de baile o un juego corto.
@@ -50,9 +58,9 @@ const Home: NextPage = () => {
     Proporcione algunas preguntas de muestra.
     `;
 
-  const generateBio = async (e: any) => {
+  const generateLessonPlan = async (e: any) => {
     e.preventDefault();
-    setGeneratedBios("");
+    setGeneratedLessonPlans("");
     setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -68,7 +76,6 @@ const Home: NextPage = () => {
       throw new Error(response.statusText);
     }
 
-    // This data is a ReadableStream
     const data = response.body;
     if (!data) {
       return;
@@ -82,127 +89,39 @@ const Home: NextPage = () => {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
       const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
+      setGeneratedLessonPlans((prev) => prev + chunkValue);
     }
-    scrollToBios();
+    scrollToLessonPlans();
     setLoading(false);
   };
 
   return (
-    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div>
       <Head>
-        <title>MagicPlan</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Your Lesson Plan Generator</title>
       </Head>
 
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <a
-          className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://samasat.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Github />
-          <p></p>
-        </a>
-        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Genera tu planeación de Clase con Navi AI
-        </h1>
-        <p className="text-slate-500 mt-5">
-          2,118 planeaciones generadas hasta ahora.
-        </p>
-        <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
-            <Image
-              src="/1-black.png"
-              width={30}
-              height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
-            />
-            <p className="text-left font-medium">
-              Escribe el tema para tu planeación de clase.{" "}
-              <span className="text-slate-500"></span>
-            </p>
-          </div>
-          <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={" por ejemplo. las celulas del cuerpo humano."}
-          />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">
-              Selecciona el Grado Escolar.
-            </p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
 
-          {!loading && (
-            <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
-            >
-              Generar Planeación &rarr;
-            </button>
-          )}
-          {loading && (
-            <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              disabled
-            >
-              <LoadingDots color="white" style="large" />
-            </button>
-          )}
+      <DropDown
+        options={gradeLevels}
+        selectedOption={gradeLevel}
+        setSelectedOption={setGradeLevel}
+        label="Grade Level"
+      />
+
+      <button onClick={generateLessonPlan}>
+        Generate Lesson Plan
+      </button>
+
+      {loading ? (
+        <LoadingDots />
+      ) : (
+        <div ref={lessonPlanRef}>
+          {parse(generatedLessonPlans)}
         </div>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-        <div className="space-y-10 my-10">
-          {/* {generatedBios && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
-                >
-                  Your generated bios
-                </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("4.")
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
-              </div>
-            </>
-          )} */}
-          {parse(generatedBios)}
-        </div>
-      </main>
+      )}
+
       <Footer />
     </div>
   );
