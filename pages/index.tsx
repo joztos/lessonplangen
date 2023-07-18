c23a52d64552371be67fb03becfc7982e3351818
Your Name <you@example.com>
2023-07-17 18:47:43 -0600

    denuevoenellrodeo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { useRef, useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Toaster, toast } from "react-hot-toast";
-import DropDown, { VibeType } from "../components/DropDown";
+import DropDown, { VibeType, AreaType } from "../components/DropDown";
 import Footer from "../components/Footer";
 import Github from "../components/GitHub";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
 import parse from "html-react-parser";
 
-const Home: NextPage = () => {
+const Home = () => {
   const [loading, setLoading] = useState(false);
   const [bio, setBio] = useState("");
   const [vibe, setVibe] = useState<VibeType>("Primero de Primaria");
+  const [area, setArea] = useState<AreaType>("Ciencias Naturales");
   const [generatedBios, setGeneratedBios] = useState<string>("");
 
   const bioRef = useRef<null | HTMLDivElement>(null);
@@ -29,27 +27,22 @@ const Home: NextPage = () => {
   }, [generatedBios]);
 
   const prompt =
-  "I am creating an app that generates class plans for students. We have various student grade levels, such as First Grade, Second Grade.. All of them have their own theme for creating a class plan. Please, create a long and perfect class plan according to the student's grade level and the theme. The student's grade is " +
-  vibe +
-  " the theme is " +
-  bio +
-  `\n. Your response must be formatted using HTML Elements for easier readability, including paragraph tags, line breaks, headings and bold titles where applicable, no need to create Full HTML Page including head, title elements. Write the previous content with the following topics.    
-  1. Early activity.
-  Provide an activation and focus activity for the students before starting the class.
-  2. Prerequisites
-  Provide the detailed knowledge required to learn the topic.
-  3. Class Theme and Objectives
-  4. Development of the Theme
-  5. Reconnect Activity
-  This allows the students to reconnect their attention to class time and prepare them emotionally for the development of the planned activities.
-  6. Class Activities
-  The activities should be prepared and designed to reinforce the new knowledge learned. They can be developed individually or collectively. It has to be productive and meaningful to promote the development of thinking skills. We recommend using an educational platform for this.
-  7. Assessment
-  Provide some sample questions.
-  `;
+    "I am creating an app that generates class plans for students. We have various student grade levels, such as " +
+    vibe +
+    ". All of them have their own theme for creating a class plan. Please create a long and perfect class plan according to the student's grade level and the theme. The student's grade is " +
+    vibe +
+    " and the theme is " +
+    bio +
+    ". Your response must be formatted using HTML Elements for easier readability, including paragraph tags, line breaks, headings, and bold titles where applicable. No need to create a full HTML page, including head and title elements. Write the previous content with the following topics:\n" +
+    "1. Early activity: Provide an activation and focus activity for the students before starting the class.\n" +
+    "2. Prerequisites: Provide the detailed knowledge required to learn the topic.\n" +
+    "3. Class Theme and Objectives.\n" +
+    "4. Development of the Theme.\n" +
+    "5. Reconnect Activity: This allows the students to reconnect their attention to class time and prepare them emotionally for the development of the planned activities.\n" +
+    "6. Class Activities: The activities should be prepared and designed to reinforce the new knowledge learned. They can be developed individually or collectively. It has to be productive and meaningful to promote the development of thinking skills. We recommend using an educational platform for this.\n" +
+    "7. Assessment: Provide some sample questions.";
 
-
-  const generateBio = async (e: any) => {
+  const generateBio = async (e: React.FormEvent) => {
     e.preventDefault();
     setGeneratedBios("");
     setLoading(true);
@@ -67,33 +60,14 @@ const Home: NextPage = () => {
       throw new Error(response.statusText);
     }
 
-    // This data is a ReadableStream
-    const data = response.body;
-    if (!data) {
-      return;
-    }
-
-    const reader = data.getReader();
-    const decoder = new TextDecoder();
-    let done = false;
-
-    while (!done) {
-      const { value, done: doneReading } = await reader.read();
-      done = doneReading;
-      const chunkValue = decoder.decode(value);
-      setGeneratedBios((prev) => prev + chunkValue);
-    }
+    const data = await response.text();
+    setGeneratedBios(data);
     scrollToBios();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
-      <Head>
-        <title>MagicPlan</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <a
@@ -113,16 +87,8 @@ const Home: NextPage = () => {
         </p>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
-            <Image
-              src="/1-black.png"
-              width={30}
-              height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
-            />
             <p className="text-left font-medium">
-              Enter the topic for your lesson plan.{" "}
-              <span className="text-slate-500"></span>
+              Enter the topic for your lesson plan.
             </p>
           </div>
           <textarea
@@ -130,27 +96,28 @@ const Home: NextPage = () => {
             onChange={(e) => setBio(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={"For example, the cells of the human body"}
+            placeholder="For example, the cells of the human body"
           />
           <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">
-              Select the School Grade.
-            </p>
+            <p className="text-left font-medium">Select the School Grade.</p>
           </div>
           <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
+            <DropDown
+              vibe={vibe}
+              setVibe={(newVibe) => setVibe(newVibe)}
+              area={area}
+              setArea={(newArea) => setArea(newArea)}
+            />
           </div>
 
-          {!loading && (
+          {!loading ? (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              onClick={generateBio}
             >
               Generate Lesson Plan &rarr;
             </button>
-          )}
-          {loading && (
+          ) : (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               disabled
@@ -166,39 +133,6 @@ const Home: NextPage = () => {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <div className="space-y-10 my-10">
-          {/* {generatedBios && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
-                >
-                  Your generated bios
-                </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("4.")
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
-                        key={generatedBio}
-                      >
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
-              </div>
-            </>
-          )} */}
           {parse(generatedBios)}
         </div>
       </main>
